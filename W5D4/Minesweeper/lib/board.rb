fc9ea6058e78f828168431e7b429885f2d5841d4
Jacob Meyer <jacobpmeyer87@gmail.com>
2019-12-20 18:05:22 -0600

    Create basic skeleton for classes

--- W5D4/Minesweeper/lib/board.rb
+++ W5D4/Minesweeper/lib/board.rb
@@ -2,5 +2,44 @@
 
 class Board
 
+  attr_reader :size
+
+  def intialize(size)
+    @grid = Array.new(size) { Array.new(size) }
+    @size = size
+  end
+
+  def [](pos)
+    row, col = pos
+    self.grid[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    self.grid[row][col] = val
+  end
+
+  def place_bombs
+    until bomb_count == 10
+      (0...size
+    end
+  end
+
+  def valid_pos?
+    if self[pos] == ""
+  end
+
+  def bomb_count
+    self.grid.flatten.count { |ele| ele == "B" }
+  end
+
+  def render
+    puts "    #{(0...9).to_a.join(' ')}"
+    puts "   ------------------"
+    @grid.each_with_index do |row, i|
+      puts "#{i} | #{row.join(" ")}"
+    end
+    puts "   ------------------"
+  end
 
 end
--- W5D4/Minesweeper/lib/minesweeper.rb
+++ W5D4/Minesweeper/lib/minesweeper.rb
@@ -5,4 +5,23 @@ require_relative "board"
 
 class Minesweeper
 
+  attr_accessor :grid
+
+  def self.new_game
+    Minesweeper.new
+  end
+
+  def initialize(size = 9)
+    @grid = Board.new(size)
+  end
+  
+  def render
+    self.board.render
+  end
+
+end
+
+if $PROGRAM_NAME == __FILE__
+  game = Minesweeper.new
+  game.run
 end
