d9c4bf02673538786e72480c6c8df9c69c3ef928
Jacob Meyer <jacobpmeyer87@gmail.com>
2019-12-31 15:42:08 -0600

    Finish class inheritance project

--- W6D3/class_inheritance/employee.rb
+++ W6D3/class_inheritance/employee.rb
@@ -1,4 +1,18 @@
-
 class Employee
+  def initialize(name,title,salary,boss)
+    @name, @title, @salary, @boss = name, title, salary, boss
+    boss.set_employee(self) unless boss.nil?
+  end
 
+  def set_employee(employee)
+    @subordinates << employee
+  end
+
+  def bonus(multiplier)
+    @salary * multiplier
+  end
+
+  def salary
+    @salary.dup
+  end
 end
--- W6D3/class_inheritance/manager.rb
+++ W6D3/class_inheritance/manager.rb
@@ -1,4 +1,35 @@
+require_relative "employee"
+require "byebug"
 
 class Manager < Employee
-  
+
+  def initialize(name,title,salary,boss)
+    super(name,title,salary,boss)
+    @subordinates = []
+  end
+
+  def subordinates_collective_salary
+    subs = 0
+    @subordinates.each { |sub|
+      if sub.is_a?(Manager)
+        subs += sub.subordinates_collective_salary
+      end
+
+      subs += sub.salary
+    }
+    subs
+  end
+
+  def bonus(multiplier)
+    multiplier * subordinates_collective_salary
+  end
 end
+
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
