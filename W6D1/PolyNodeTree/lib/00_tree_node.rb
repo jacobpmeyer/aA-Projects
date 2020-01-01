1c801ceddc3991e784668dcb01f3d55fef52267a
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-01-01 13:32:50 -0600

    Finish PolyNodeTree with DFS and BFS as Searchable module

--- W6D1/PolyNodeTree/lib/00_tree_node.rb
+++ W6D1/PolyNodeTree/lib/00_tree_node.rb
@@ -1,7 +1,60 @@
+module Searchable
+  def dfs(target = nil, &prc)
+    prc ||= Proc.new { |a, b| a == b }
+    return self if prc.call(self.value, target)
+    self.children.each do |child|
+      result = child.dfs(target, &prc)
+      return result unless result.nil?
+    end
+    nil
+  end
+
+  def bfs(target = nil, &prc)
+    prc ||= Proc.new { |a, b| a == b }
+    queue = [self]
+    until queue.empty?
+      node = queue.shift
+      return node if prc.call(node.value, target)
+      queue.concat(node.children)
+    end
+    nil
+  end
+end
+
 class PolyTreeNode
- 
-  def initialize
-    
+  attr_reader :value, :parent
+  include Searchable
+  
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def children
+    @children.dup
+  end
+
+  def parent=(parent_node)
+    self.parent._children.delete(self) unless self.parent.nil?
+    @parent = parent_node
+    self.parent._children << self unless parent_node.nil?
+  end
+
+  def add_child(node)
+    node.parent = self
+  end
+  
+  def remove_child(node)
+    unless self.children.include?(node)
+      raise ArgumentError("#{node} is not a child of #{self}")
+    end
+    node.parent = nil
+  end
+
+  protected
+  def _children
+    @children
   end
 
 end
