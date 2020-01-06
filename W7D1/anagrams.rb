622e6e889a3c399f64c1a9d62b5de2f3a9fe0230
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-01-06 17:12:49 -0600

    Start two_sum

--- W7D1/anagrams.rb
+++ W7D1/anagrams.rb
@@ -1,10 +1,11 @@
 
-
+# time complexity => O(n!)
 def first_anagram?(str1, str2)
     perms = str1.split("").permutation.to_a
     perms.include?(str2)
 end
 
+# time complexity => O(n^2)
 def second_anagram?(str1, str2)
     str1.each_char do |char|
         if str2.include?(char)
@@ -18,6 +19,7 @@ def second_anagram?(str1, str2)
     str2 == ''
 end
 
+# time complexity => O(nlog(n))
 def third_anagram?(str1, str2)
   str1.chars.sort == str2.chars.sort
 end
@@ -30,23 +32,24 @@ end
 #   hash1 == hash2
 # end
 
-# def bonus_anagram?(str1, str2)
-#   hash = Hash.new(0)
-#   str1.each_char { |char| hash[char] += 1 }
-#   str2.each_char { |char| hash[char] -= 1 }
-#   vals = hash.values
-#   vals.all? { |ele| ele == 0 }
+def bonus_anagram?(str1, str2)
+  hash = Hash.new(0)
+  str1.each_char { |char| hash[char] += 1 }
+  str2.each_char { |char| hash[char] -= 1 }
+  vals = hash.values
+  vals.all? { |ele| ele == 0 }
+end
+
+# time complexity => O(n)
+# def fourth_anagram?(str1, str2)
+#   helper(str1) == helper(str2)
 # end
 
-def fourth_anagram?(str1, str2)
-  helper(str1) == helper(str2)
-end
-
-def helper(str)
-  hash = Hash.new(0)
-  str.each_char { |char| hash[char] += 1 }
-  hash
-end
+# def helper(str)
+#   hash = Hash.new(0)
+#   str.each_char { |char| hash[char] += 1 }
+#   hash
+# end
 # Time complexity => O(n)
 
 # p bonus_anagram?("gizmo", "sally")    #=> false
