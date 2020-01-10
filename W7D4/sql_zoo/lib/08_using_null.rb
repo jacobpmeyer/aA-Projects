76cb327513571941c687949e54fdd77564bea413
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-01-09 19:11:39 -0600

    Finish W7D4

--- W7D4/sql_zoo/lib/08_using_null.rb
+++ W7D4/sql_zoo/lib/08_using_null.rb
@@ -18,6 +18,14 @@ require_relative './sqlzoo.rb'
 def null_dept
   # List the teachers who have NULL for their department.
   execute(<<-SQL)
+  SELECT 
+  teachers.name
+  FROM
+  teachers
+  LEFT OUTER JOIN 
+  depts ON teachers.dept_id = depts.id
+  WHERE
+  teachers.dept_id IS NULL
   SQL
 end
 
@@ -25,6 +33,12 @@ def all_teachers_join
   # Use a type of JOIN that will list all teachers and their department,
   # even if the department in NULL/nil.
   execute(<<-SQL)
+  SELECT
+  teachers.name, depts.name 
+  FROM 
+  teachers
+  LEFT OUTER JOIN depts
+  ON depts.id = teachers.dept_id 
   SQL
 end
 
@@ -33,6 +47,12 @@ def all_depts_join
   # NB: you can avoid RIGHT OUTER JOIN (and just use LEFT) by swapping
   # the FROM and JOIN tables.
   execute(<<-SQL)
+  SELECT
+  teachers.name, depts.name
+  FROM 
+  depts
+  LEFT OUTER JOIN teachers
+  ON teachers.dept_id = depts.id  
   SQL
 end
 
