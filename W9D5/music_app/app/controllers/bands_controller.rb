99bb929a1eeb6c2086cb913932c3003a90defc24
Jacob Meyer <jacobmeyer@pop-os.localdomain>
2020-01-27 17:29:24 -0600

    Finish Band resource

--- W9D5/music_app/app/controllers/bands_controller.rb
+++ W9D5/music_app/app/controllers/bands_controller.rb
@@ -4,12 +4,17 @@ class BandsController < ApplicationController
     render :index
   end
 
+  def show
+    @band = Band.find(params[:id])
+    render :show
+  end
+
   def new
     @band = Band.new
     render :new
   end
 
-  def show
+  def create
     @band = Band.new(band_params)
 
     if @band.save
@@ -21,15 +26,25 @@ class BandsController < ApplicationController
   end
 
   def edit
-
+    @band = Band.find(params[:id])
+    render :edit
   end
 
   def update
+    @band = Band.find(params[:id])
 
+    if @band.update(band_params)
+      redirect_to band_url(@band.id)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
-
+    band = Band.find(params[:id])
+    band.destroy
+    redirect_to bands_url
   end
 
   private
