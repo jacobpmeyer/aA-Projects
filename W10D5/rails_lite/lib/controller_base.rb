182d12d2569d3100acaafc5774ea9ff9f0e02d4d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-02-01 22:43:23 -0600

    Work on rails lite

---
+++ W10D5/rails_lite/lib/controller_base.rb
@@ -0,0 +1,54 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require 'byebug'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req, @res = req, res
+    @params = req.params
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "double render" if @already_built_response
+    @already_built_response = true
+    res.set_header("Location", url) 
+    res.status = 302
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "double render" if @already_built_response
+    @already_built_response = true
+    res.set_header("Content-Type", content_type)
+    res.write(content)
+     
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name) #render :show
+
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
+
