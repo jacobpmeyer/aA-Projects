03be363d33a466c7c42fc4e7dda99480d823df63
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-23 11:51:28 -0500

    Complete bubbleSort

--- W22D4/bubble_sort_project/lib/bubble_sort.js
+++ W22D4/bubble_sort_project/lib/bubble_sort.js
@@ -1,13 +1,27 @@
 function swap(array, idx1, idx2) {
-
+  let temp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = temp;
+  return array;
 }
 
 function bubbleSort(array) {
+  let swapped = true;
 
+  while (swapped) {
+    swapped = false;
+
+    for (let index = 0; index < array.length - 1; index++) {
+      if (array[index] > array[index + 1]) {
+        swap(array, index, index + 1);
+        swapped = true;
+      }
+    }
+  }
+  return array;
 }
 
-
 module.exports = {
-    bubbleSort,
-    swap
+  bubbleSort,
+  swap,
 };
