3a0adb7840209ca768741b2162ad7e19415d66d5
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-24 13:27:07 -0500

    Finish radix sort

--- W22D5/radix_sort_project/lib/radix_sort.js
+++ W22D5/radix_sort_project/lib/radix_sort.js
@@ -1,7 +1,45 @@
 function radixSort(arr) {
+  if (!Array.isArray(arr)) return null;
+  if (arr.length === 1) return arr;
+  if (arr.length === 0) return [];
 
+  for (i = 0; i < getMaxDigits(arr); i++) {
+    let buckets = new Array(10).fill().map(() => new Array());
+    for (let j = 0; j < arr.length; j++) {
+      let digit = getDigitFrom(arr[j], i);
+      //   console.log(j, i);
+      buckets[digit].push(arr[j]);
+    }
+    arr = [].concat(...buckets);
+  }
+  return arr;
 }
 
+const getDigitFrom = (num, place) =>
+  Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+// function getDigitFrom(num, place) {
+//   let n = String(num).split("").reverse()[place];
+//   return Number(n);
+// }
+
+function getIntLength(num) {
+  return String(num).length;
+}
+
+function getMaxDigits(nums) {
+  let maxDigit = String(nums[0]).length;
+
+  for (let i = 0; i < nums.length; i++) {
+    const el = String(nums[i]).length;
+    if (el > maxDigit) maxDigit = el;
+  }
+
+  return maxDigit;
+}
+
+console.log(radixSort([4, 9, 0, 23, 15, 100, 66, 41, 5, 10]));
+
 module.exports = {
-    radixSort
+  radixSort,
 };
