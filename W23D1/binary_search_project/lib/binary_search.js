e54145b1e163e466423b39dda8f678494748b200
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-27 12:45:24 -0500

    Finish bsearch

--- W23D1/binary_search_project/lib/binary_search.js
+++ W23D1/binary_search_project/lib/binary_search.js
@@ -1,13 +1,39 @@
-function binarySearch(array, target) {
+function binarySearch(arr, target) {
+  if (!arr.length) return false;
 
+  const midIdx = Math.floor(arr.length / 2);
+
+  if (arr[midIdx] === target) return true;
+
+  const left = arr.slice(0, midIdx);
+  const right = arr.slice(midIdx + 1);
+
+  if (arr[midIdx] > target) {
+    return binarySearch(left, target);
+  } else {
+    return binarySearch(right, target);
+  }
 }
 
-function binarySearchIndex(array, target) {
+function binarySearchIndex(arr, target) {
+  if (!arr.length) return -1;
 
+  const midIdx = Math.floor(arr.length / 2);
+
+  if (arr[midIdx] === target) return arr.indexOf(arr[midIdx]);
+
+  const left = arr.slice(0, midIdx);
+  const right = arr.slice(midIdx + 1);
+
+  if (arr[midIdx] > target) {
+    return binarySearchIndex(left, target);
+  } else {
+    const rightResult = binarySearchIndex(right, target);
+    return rightResult !== -1 ? left.length + rightResult + 1 : -1;
+  }
 }
-
 
 module.exports = {
-    binarySearch,
-    binarySearchIndex
+  binarySearch,
+  binarySearchIndex,
 };
