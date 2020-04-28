16b2a47dd8eaa72c7e52338f61bef90d22011d44
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-28 18:38:58 -0500

    Finish stack queue problem

--- W23D2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ W23D2/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -6,11 +6,11 @@
 // Prompt:
 // -------
 //
-// Iterate over a Singly Linked List of primitives backwards. When finished, 
-// return a string representing the original linked list's values backwards 
+// Iterate over a Singly Linked List of primitives backwards. When finished,
+// return a string representing the original linked list's values backwards
 // in the following format:
 //
-//                             'A -> B -> C -> D' 
+//                             'A -> B -> C -> D'
 //
 // ------------
 // Constraints:
@@ -27,8 +27,82 @@
 // -----------
 
 function iterateAcrossLinkedListBackwards(linkedList) {
-    // TODO: Implement the iterateAcrossLinkedListBackwards function here
+  // TODO: Implement the iterateAcrossLinkedListBackwards function here
 
+  if (!linkedList.length) return "";
+  const stack = new Stack();
+  let node = linkedList.head.value;
+  let str = "";
+
+  //   for (let i = 0; i < linkedList.length; i++) {
+  //     stack.push(node);
+  //     node = node.next;
+  //     // console.log("node: ", node);
+  //   }
+
+  while (node) {
+    stack.push(node);
+    node = node.next;
+  }
+
+  while (stack.size() > 1) {
+    const current = stack.pop();
+    str += `${current} -> `;
+  }
+
+  str += `${stack.pop()}`;
+
+  return str;
+}
+
+// Stack
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    let tempNode;
+    if (!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      tempNode = this.top;
+      this.top = newNode;
+      this.top.next = tempNode;
+    }
+    this.length++;
+    return this.length;
+    // return ++this.length;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const temp = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return temp.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
