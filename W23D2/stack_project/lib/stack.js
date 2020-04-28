00288864c217d5f7422f2b62266af9b649141f10
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-28 14:39:09 -0500

    Finish balanced parens

--- W23D2/stack_project/lib/stack.js
+++ W23D2/stack_project/lib/stack.js
@@ -12,7 +12,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -21,11 +21,51 @@
 // -----------
 
 class Node {
-
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
 }
 
 class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
 
+  push(val) {
+    let newNode = new Node(val);
+    let tempNode;
+    if (!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      tempNode = this.top;
+      this.top = newNode;
+      this.top.next = tempNode;
+    }
+    this.length++;
+    return this.length;
+    // return ++this.length;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    const temp = this.top;
+    this.top = this.top.next;
+    this.length--;
+    return temp.value;
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
