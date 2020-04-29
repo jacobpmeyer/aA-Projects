edbc53b4398df67b01dcba09545fc42bf12857a8
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-29 11:07:13 -0500

    Finish tree order and node problems

--- W23D3/tree_order_project/lib/tree_order.js
+++ W23D3/tree_order_project/lib/tree_order.js
@@ -1,13 +1,18 @@
 function inOrderArray(root) {
-
+  if (!root) return [];
+  return inOrderArray(root.left)
+    .concat([root.val])
+    .concat(inOrderArray(root.right));
 }
 
 function postOrderArray(root) {
-
+  if (!root) return [];
+  return postOrderArray(root.left)
+    .concat(postOrderArray(root.right))
+    .concat([root.val]);
 }
 
-
 module.exports = {
-    inOrderArray,
-    postOrderArray
+  inOrderArray,
+  postOrderArray,
 };
