49052146b51fcbdcc28dc30b3376c913e2fb3485
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-29 12:06:31 -0500

    Finish dfs & bfs problems

--- W23D3/depth_breadth_first_project/lib/breadth_first_array.js
+++ W23D3/depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,7 +1,14 @@
 function breadthFirstArray(root) {
-
+  let stack = [root];
+  let i = 0;
+  for (let i = 0; i < stack.length; i++) {
+    node = stack[i];
+    if (node.left) stack.push(stack[i].left);
+    if (node.right) stack.push(stack[i].right);
+  }
+  return stack.map((node) => node.val);
 }
 
 module.exports = {
-    breadthFirstArray
+  breadthFirstArray,
 };
--- W23D3/depth_breadth_first_project/lib/depth_first_search.js
+++ W23D3/depth_breadth_first_project/lib/depth_first_search.js
@@ -1,8 +1,12 @@
 function depthFirstSearch(root, targetVal) {
-
+  if (!root) return null;
+  if (root.val === targetVal) return root;
+  left = depthFirstSearch(root.left, targetVal);
+  if (left) return left;
+  right = depthFirstSearch(root.right, targetVal);
+  return right;
 }
 
-
 module.exports = {
-    depthFirstSearch
+  depthFirstSearch,
 };
