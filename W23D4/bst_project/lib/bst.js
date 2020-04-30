0c327d9964aebf4f99d74a5615d230aea8e834e8
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-04-30 11:39:31 -0500

    Finish bst
    Co-authored-by: Randy Lough <randallmlough@gmail.com>

--- W23D4/bst_project/lib/bst.js
+++ W23D4/bst_project/lib/bst.js
@@ -1,17 +1,48 @@
 class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-
 class BST {
-   
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, root = this.root) {
+    if (!root) {
+      return (this.root = new TreeNode(val));
+    }
+    if (val >= root.val) {
+      if (!root.right) root.right = new TreeNode(val);
+      else this.insert(val, root.right);
+    } else {
+      if (!root.left) root.left = new TreeNode(val);
+      else this.insert(val, root.left);
+    }
+  }
+
+  searchRecur(val, root = this.root) {
+    if (!root) return false;
+    if (val === root.val) return true;
+    else if (val > root.val) return this.searchRecur(val, root.right);
+    else return this.searchRecur(val, root.left);
+  }
+
+  searchIter(val) {
+    let node = this.root;
+    while (node) {
+      if (node.val === val) return true;
+      if (val > node.val) node = node.right;
+      else node = node.left;
+    }
+    return false;
+  }
 }
 
 module.exports = {
-    TreeNode,
-    BST
+  TreeNode,
+  BST,
 };
