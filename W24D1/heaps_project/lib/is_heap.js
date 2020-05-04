4a0e25389605ffa34caec82ff7b112ee01ad086c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-04 12:34:10 -0500

    Finish is_heap and max_heap

--- W24D1/heaps_project/lib/is_heap.js
+++ W24D1/heaps_project/lib/is_heap.js
@@ -1,9 +1,21 @@
 // you may assume that the array will always have a null element at the 0-th index
-function isMaxHeap(array, idx=1) {
+function isMaxHeap(array, idx = 1) {
+  if (idx === array.length - 1) return true;
+  if (array.length === 1) return true;
+  let leftIdx = idx * 2;
+  let rightIdx = idx * 2 + 1;
 
+  let leftVal = array[leftIdx];
+  let rigthVal = array[rightIdx];
+
+  if (leftVal === undefined) leftVal = -Infinity;
+  if (rigthVal === undefined) rigthVal = -Infinity;
+
+  if (leftVal > array[idx] || rigthVal > array[idx]) return false;
+
+  return isMaxHeap(array, idx + 1);
 }
 
-
 module.exports = {
-    isMaxHeap
+  isMaxHeap,
 };
--- W24D1/heaps_project/lib/max_heap.js
+++ W24D1/heaps_project/lib/max_heap.js
@@ -1,7 +1,76 @@
 class MaxHeap {
-    
+  constructor() {
+    this.array = [null];
+  }
+
+  getParent(idx) {
+    return Math.floor(idx / 2);
+  }
+
+  getLeftChild(idx) {
+    return idx * 2;
+  }
+
+  getRightChild(idx) {
+    return idx * 2 + 1;
+  }
+
+  insert(val) {
+    this.array.push(val);
+    this.siftUp(this.array.length - 1);
+  }
+
+  siftUp(idx) {
+    if (this.array.length === 1) return;
+    let parentIdx = this.getParent(idx);
+    let parentVal = this.array[parentIdx];
+    let currentVal = this.array[idx];
+    if (currentVal > parentVal && parentVal !== null) {
+      [this.array[parentIdx], this.array[idx]] = [
+        this.array[idx],
+        this.array[parentIdx],
+      ];
+      this.siftUp(parentIdx);
+    }
+  }
+
+  deleteMax() {
+    if (this.array.length === 1) return null;
+    if (this.array.length === 2) return this.array.pop();
+    let max = this.array[1];
+    this.array[1] = this.array.pop();
+    this.siftDown(1);
+    // let max = this.array[1];
+    // this.array.splice(1, 1);
+
+    return max;
+  }
+
+  siftDown(idx) {
+    if (this.array.length === 1) return;
+    let currentVal = this.array[idx];
+    let rightIdx = this.getRightChild(idx);
+    let leftIdx = this.getLeftChild(idx);
+    let rightVal = this.array[rightIdx];
+    let leftVal = this.array[leftIdx];
+    if (rightVal === undefined) rightVal = -Infinity;
+    if (leftVal === undefined) leftVal = -Infinity;
+    if (currentVal > rightVal && currentVal > leftVal) return;
+    let swapIdx;
+    if (rightVal < leftVal) {
+      swapIdx = leftIdx;
+    } else {
+      swapIdx = rightIdx;
+    }
+
+    [this.array[swapIdx], this.array[idx]] = [
+      this.array[idx],
+      this.array[swapIdx],
+    ];
+    this.siftDown(swapIdx);
+  }
 }
 
 module.exports = {
-    MaxHeap
+  MaxHeap,
 };
