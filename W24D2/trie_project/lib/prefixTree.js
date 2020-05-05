4840a0adb284558271ffdc78e2b1da88a3a2c154
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-05 12:04:49 -0500

    Finish leetcode problem

--- W24D2/trie_project/lib/prefixTree.js
+++ W24D2/trie_project/lib/prefixTree.js
@@ -1,21 +1,67 @@
 /**
  * Initialize your data structure here.
  */
-class Node {}
+class Node {
+  constructor() {
+    this.children = {};
+    this.isTerminal = false;
+  }
+}
 
 class Trie {
-  insert(word) {}
+  constructor() {
+    this.root = new Node();
+  }
 
-  search(word) {}
+  insert(word, root = this.root) {
+    let letter = word[0];
 
-  startsWith(prefix) {}
+    if (!(letter in root.children)) {
+      root.children[letter] = new Node();
+    }
+
+    if (word.length === 1) {
+      root.children[letter].isTerminal = true;
+    } else {
+      this.insert(word.slice(1), root.children[letter]);
+    }
+  }
+
+  search(word, root = this.root) {
+    if (word.length === 0) {
+      if (root.isTerminal) return true;
+      else return false;
+    }
+
+    let letter = word[0];
+
+    if (letter in root.children) {
+      return this.search(word.slice(1), root.children[letter]);
+    } else {
+      return false;
+    }
+  }
+
+  startsWith(prefix, root = this.root) {
+    if (prefix.length === 0) {
+      return true;
+    }
+
+    let letter = prefix[0];
+
+    if (letter in root.children) {
+      return this.startsWith(prefix.slice(1), root.children[letter]);
+    } else {
+      return false;
+    }
+  }
 }
 
 // let trie = new Trie();
 
-// trie.insert("apple");
-// trie.search("apple");   // returns true
-// trie.search("app");     // returns false
-// trie.startsWith("app"); // returns true
-// trie.insert("app");
-// trie.search("app");     // returns true
+// console.log(trie.insert("apple"));
+// console.log(trie.search("apple"));   // returns true
+// console.log(trie.search("app"));     // returns false
+// console.log(trie.startsWith("app")); // returns true
+// console.log(trie.insert("app"));
+// console.log(trie.search("app"));     // returns true
