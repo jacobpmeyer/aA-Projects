f32da9b9ccbab9d9dde2dd0f732f3bbccf234142
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-05 11:57:03 -0500

    Start prefixTree.js

---
+++ W24D2/trie_project/lib/prefixTree.js
@@ -0,0 +1,21 @@
+/**
+ * Initialize your data structure here.
+ */
+class Node {}
+
+class Trie {
+  insert(word) {}
+
+  search(word) {}
+
+  startsWith(prefix) {}
+}
+
+// let trie = new Trie();
+
+// trie.insert("apple");
+// trie.search("apple");   // returns true
+// trie.search("app");     // returns false
+// trie.startsWith("app"); // returns true
+// trie.insert("app");
+// trie.search("app");     // returns true
